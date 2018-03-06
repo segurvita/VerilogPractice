13f7a6c3c29639f03905cdc13629fdffa696133b
segur <segur.opus@gmail.com>
2018-03-06 19:07:52 +0900

    コメント

--- lesson/counter.v
+++ lesson/counter.v
@@ -5,20 +5,31 @@
 *****************************************************/
 
 module count8( clk, rst, start, cout );
-input clk;
-input rst;
+input clk;          //クロック
+input rst;          //リセット
 input start;
 output [2:0] cout;
 reg    [2:0] cout;
 
+//回路は常に動いている状態なのでalways
+//@は()内に変化があったら発火
+//リセットは負論理
 always @(posedge clk or negedge rst ) begin
+    //リセットが0なら
     if( rst == 1'b0 ) begin
+        //カウントを0にする
         cout <= 3'd0;
     end
+
+    //スタートが1なら
     else if( start == 1'b1 ) begin
+        //カウントを1にする
         cout <= 1'b1;
     end
+
+    //上記以外
     else begin
+        //カウントを1増やす
         cout <= cout + 3'd1;
     end
 end
